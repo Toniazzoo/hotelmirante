8b5f03181e7623d5c8c879908a1477befd608af7
Toniazzoo <eduardo.ta@aluno.ifsc.edu.br>
2024-08-30 10:30:56 -0300

    atualizado

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,20 +8,14 @@ export default defineConfig({
 
   themeConfig: {
     nav: [
-      { text: 'Introdução', link: '1_Introducao' },
-      { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
-      { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
-      { text: 'Diagrama de Caso de Uso', link: '/4_Diagrama_Caso_de_Uso' },
-      { text: 'Diagrama de Classe', link: '/5_Diagrama_de_Classe' },
-      { text: 'Cronograma', link: '/6_Cronograma' },
-      { text: 'Riscos', link: '/7_Riscos' },
-      { text: 'Considerações Finais', link: '/8_Consideracoes' },
-      { text: 'Protótipos de Tela', link: '/Prototipagens_de_Tela' },
+      { text: 'Example', link: '/example' },
+      
     ],
 
     sidebar: [
       {
         items: [
+          { text: 'Example', link: '/example' },
           { text: 'Introdução', link: '1_Introducao' },
           { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
           { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
--- docs/Prototipagens_de_Tela.md
+++ docs/Prototipagens_de_Tela.md
@@ -1,1 +1,23 @@
 ![Tela inicial](./img/telainicial.png)
+
+![Criar contal](./img/criarconta.png)
+
+![Escolher quarto](./img/escolherquarto.png)
+
+![Login](./img/login.png)
+
+![Pagamento 1](./img/pagamento1.png)
+
+![Pagamento 2](./img/pagamento2.png)
+
+![Pagamento 3](./img/pagamento3.png)
+
+![Pesquisar Hotel](./img/pesquisarhotel.png)
+
+![Recuperar senha](./img/recsenha.png)
+
+![Reservas 1](./img/reservas1.png)
+
+![Reservas 2](./img/reservas2.png)
+
+![Reservas 3](./img/reservas3.png)
