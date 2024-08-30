c4c316e5dabf7442dec063aab29bc37735ed3507
Toniazzoo <eduardo.ta@aluno.ifsc.edu.br>
2024-08-30 09:48:35 -0300

    atualizado

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,24 +3,19 @@ import { defineConfig } from 'vitepress';
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
-  title: 'Hotel Mirante',
-  description: 'Melhor Software para hotéis',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
 
   themeConfig: {
     nav: [
-      { text: 'Introdução', link: '1_Introducao' },
-      { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
-      { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
-      { text: 'Diagrama de Caso de Uso', link: '/4_Diagrama_Caso_de_Uso' },
-      { text: 'Diagrama de Classe', link: '/5_Diagrama_de_Classe' },
-      { text: 'Cronograma', link: '/6_Cronograma' },
-      { text: 'Riscos', link: '/7_Riscos' },
-      { text: 'Considerações Finais', link: '/8_Consideracoes' },
+      { text: 'Example', link: '/example' },
+      
     ],
 
     sidebar: [
       {
         items: [
+          { text: 'Example', link: '/example' },
           { text: 'Introdução', link: '1_Introducao' },
           { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
           { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
