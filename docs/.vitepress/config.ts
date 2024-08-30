37fb66e692a3448abde40ac2a304935f542796ff
Toniazzoo <eduardo.ta@aluno.ifsc.edu.br>
2024-08-30 00:38:56 -0300

    navegação de telas

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,30 +3,24 @@ import { defineConfig } from 'vitepress';
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
+  title: 'Hotel Mirante',
+  description: 'Melhor Software para hotéis',
 
   themeConfig: {
     nav: [
-      { text: 'Example', link: '/example' },
-      
-
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
-
-      // ...
+      { text: 'Introdução', link: '1_Introducao' },
+      { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
+      { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
+      { text: 'Diagrama de Caso de Uso', link: '/4_Diagrama_Caso_de_Uso' },
+      { text: 'Diagrama de Classe', link: '/5_Diagrama_de_Classe' },
+      { text: 'Cronograma', link: '/6_Cronograma' },
+      { text: 'Riscos', link: '/7_Riscos' },
+      { text: 'Considerações Finais', link: '/8_Consideracoes' },
     ],
 
     sidebar: [
       {
         items: [
-          { text: 'Example', link: '/example' },
           { text: 'Introdução', link: '1_Introducao' },
           { text: 'Descrição do Projeto', link: '/2_Descricao_projeto' },
           { text: 'Principais Recursos', link: '/3_Principais_Recursos' },
@@ -35,7 +29,6 @@ export default defineConfig({
           { text: 'Cronograma', link: '/6_Cronograma' },
           { text: 'Riscos', link: '/7_Riscos' },
           { text: 'Considerações Finais', link: '/8_Consideracoes' },
-          
         ],
       },
     ],
